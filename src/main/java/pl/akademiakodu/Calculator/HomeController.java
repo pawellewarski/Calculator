90f1a273cfa101e10c3c1796eda4bb138a41bbc1
pawellewarski <pawellewarski@gmail.com>
2018-08-26 13:50:54 +0200

    calculator functions in controller added

--- src/main/java/pl/akademiakodu/Calculator/HomeController.java
+++ src/main/java/pl/akademiakodu/Calculator/HomeController.java
@@ -29,6 +29,30 @@ public class HomeController {
             double result = calculator.add(Double.parseDouble(first_number), Double.parseDouble(second_number));
             map.put("result", result);
         }
+        if(options.equals("subtraction")){
+            double result = calculator.subtraction(Double.parseDouble(first_number), Double.parseDouble(second_number));
+            map.put("result", result);
+        }
+        if(options.equals("multiplication")){
+            double result = calculator.multiplication(Double.parseDouble(first_number), Double.parseDouble(second_number));
+            map.put("result", result);
+        }
+        if(options.equals("division")){
+            double result = calculator.division(Double.parseDouble(first_number), Double.parseDouble(second_number));
+            map.put("result", result);
+        }
+        if(options.equals("power")){
+            double result = calculator.power(Double.parseDouble(first_number), Double.parseDouble(second_number));
+            map.put("result", result);
+        }
+        if(options.equals("max")){
+            double result = calculator.max(Double.parseDouble(first_number), Double.parseDouble(second_number));
+            map.put("result", result);
+        }
+        if(options.equals("min")){
+            double result = calculator.min(Double.parseDouble(first_number), Double.parseDouble(second_number));
+            map.put("result", result);
+        }
 
         return "result";
     }
